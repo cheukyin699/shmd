03b31f441633bd25859bce850346601ab14c11ad
Cheuk Yin Ng <chucksys88@gmail.com>
2022-08-01 17:01:48 -0700

    server: media scanner wip

--- shmd-server/src/models.rs
+++ shmd-server/src/models.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 pub struct Media {
     pub id: Option<usize>,
     pub title: String,
@@ -5,4 +7,25 @@ pub struct Media {
     pub album: Option<String>,
     pub location: String,
     pub genreid: Option<usize>,
+}
+
+impl Media {
+    /**
+     * Read file metadata to create structure.
+     */
+    pub fn from_path(p: &Path) -> Self {
+        let title = String::from("No title");
+        let album = Some(String::from("Giants"));
+        let artist = Some(String::from("V O E"));
+        let location = p.display().to_string();
+
+        Media {
+            id: None,
+            title,
+            artist,
+            album,
+            location,
+            genreid: None,
+        }
+    }
 }
--- shmd-server/src/scanner.rs
+++ shmd-server/src/scanner.rs
@@ -1,31 +1,52 @@
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::collections::VecDeque;
 
-const AUDIO_EXTS: [&'static str] = [
+use tokio_postgres::Client;
+
+use crate::db;
+use crate::models::Media;
+
+const AUDIO_EXTS: [&str; 7] = [
     "mp3", "m4a", "ogg", "oga", "opus", "wav", "webm",
 ];
 
-async fn is_audio_file(p: &fs::DirEntry) -> bool {
+/**
+ * Check that a directory entry is an audio file.
+ */
+fn is_audio_file(p: &fs::DirEntry) -> bool {
     if let Some(ext) = p.path().extension() {
-        AUDIO_EXTS.contains(ext)
+        if let Some(s) = ext.to_str() {
+            AUDIO_EXTS.contains(&s)
+        } else {
+            false
+        }
     } else {
         false
     }
 }
 
-async fn collect_audio_files(root_folder: String) -> Vec<Path> {
+/**
+ * Collect a list of audio files given a root folder.
+ *
+ * Use DFS to check folder for files.
+ */
+fn collect_audio_files(root_folder: String) -> Vec<PathBuf> {
     let mut paths = vec![];
     let mut to_check = vec![PathBuf::from(root_folder)];
 
     // DFS to try and decrease vector resizing (as opposed to BFS)
     while to_check.len() > 0 {
         let x = to_check.pop().unwrap();
-        for entry in fs::read_dir(x.path()) {
-            if entry.is_dir() {
-                to_check.push(entry.path());
-            } else if entry.is_file() && is_audio_file(&entry) {
-                paths.push(entry.path());
+        if let Ok(rd) = fs::read_dir(x.as_path()) {
+            for entry in rd {
+                if let Ok(entry) = entry {
+                    let p = entry.path();
+                    if p.is_dir() {
+                        to_check.push(p);
+                    } else if p.is_file() && is_audio_file(&entry) {
+                        paths.push(p);
+                    }
+                }
             }
         }
     }
@@ -33,7 +54,30 @@ async fn collect_audio_files(root_folder: String) -> Vec<Path> {
     paths
 }
 
-pub async fn scan_files(root_folder: String) {
+/**
+ * Scan files.
+ *
+ * Add audio file metadata into database if they don't exist there. Remove rows from database if we
+ * see that the file doesn't exist on the filesystem.
+ */
+pub async fn scan_files(client: &Client, root_folder: String) {
+    let files = collect_audio_files(root_folder);
+
     // Look for files in root folder that aren't in the database (or need updating in the database)
+    for f in &files {
+        let s = f.display().to_string();
+
+        match db::file_in_db(client, &s).await {
+            Ok(true) => continue,
+            Err(e) => eprintln!("{}", e),
+            _ => {},
+        }
+
+        let m = Media::from_path(f);
+        if let Err(e) = db::insert_media(client, &m).await {
+            eprintln!("{}", e);
+        }
+    }
+
     // Look for files in database that aren't in the root folder
 }
