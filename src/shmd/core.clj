f805f2b72b94761d92f64a3401944d7d1118bcee
Cheuk Yin Ng <chucksys88@gmail.com>
2023-01-28 18:37:25 -0800

    feat: add more skelly routes and add default middleware

--- src/shmd/core.clj
+++ src/shmd/core.clj
@@ -3,15 +3,25 @@
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.util.response :refer [response]]
+            [ring.middleware.defaults :use [wrap-defaults api-defaults]]
+            [ring.middleware.params :use [wrap-params]]
+            [ring.middleware.keyword-params :use [wrap-keyword-params]]
             [ring.middleware.json :use [wrap-json-response]]))
 
 (defroutes handler
   (GET "/" [] (response {:a 32 :b 42}))
+  (GET "/thumbnail/:album" [album] (response {:album album}))
+  (PATCH "/media" [& params] (response params))
+  (GET "/media" [& params] (response {:data [] :params params}))
+  (GET "/media/:id" [id] (response {:id id}))
+  (GET "/status" [] (response {:media-in-db 0}))
   (route/not-found "not found"))
 
 (def app
-  (-> handler
-      (wrap-json-response)))
+  (-> #'handler
+      (wrap-keyword-params)
+      (wrap-json-response)
+      (wrap-defaults api-defaults)))
 
 (defn -main
   [& args]
